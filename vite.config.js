5305ee5080bb6631c1871bd4042033cae93a4b7a
nuelsha <mezemirnuel@gmail.com>
2025-08-14 07:00:30 -0700

    done

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import gltf from 'vite-plugin-gltf'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), gltf()],
 })
